c9a5889d4df1a2bb1673403caa9320162f3fbce4
sammieGHA <oo650869@gmail.com>
2026-08-16 18:35:30 -0500

    camera lerp tween thing follow thing

--- src/states/PlayState.hx
+++ src/states/PlayState.hx
@@ -3,12 +3,15 @@ package states;
 import flixel.FlxG;
 import flixel.FlxState;
 import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 import objects.Horse;
 import objects.Map.MapData;
 import objects.Map;
 
 class PlayState extends FlxState
 {
+	var selectedHorse:Horse;
 	var horses:FlxTypedGroup<Horse>;
 	var map:Map;
 
@@ -29,10 +32,38 @@ class PlayState extends FlxState
 		}
 
 		FlxG.sound.playMusic(Paths.music('mus-1'));
+		FlxG.camera.setScrollBounds(0, FlxG.width, 0, FlxG.height);
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		if (FlxG.mouse.justPressed)
+		{
+			var daClickedHorse:Horse = null;
+
+			for (horse in horses.members)
+			{
+				if (FlxG.mouse.overlaps(horse))
+				{
+					daClickedHorse = horse;
+					break;
+				}
+			}
+
+			if (daClickedHorse != null)
+			{
+				selectedHorse = daClickedHorse;
+				FlxG.camera.follow(daClickedHorse, LOCKON);
+				FlxTween.tween(FlxG.camera, {zoom: 1.5}, 0.4, {ease: FlxEase.quadInOut});
+			}
+			else
+			{
+				selectedHorse = null;
+				FlxG.camera.follow(null);
+				FlxTween.tween(FlxG.camera, {zoom: 1}, 0.4, {ease: FlxEase.quadInOut});
+			}
+		}
 	}
 }
