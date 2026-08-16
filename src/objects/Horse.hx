9b11f1ed1d7cea86180422af36d4f1b6af9633bd
salieri-alex <somerandomgithubacc@gmail.com>
2026-08-16 23:13:43 +0100

    ok  ma

--- src/objects/Horse.hx
+++ src/objects/Horse.hx
@@ -17,16 +17,50 @@ class Horse extends FlxSprite
 		velocity.y = 50;
 	}
 
+	var speed:Float = 100;
+	// well its a way to stop phasing through blue walls but clearly it sucks at doing that
+	var previousX:Float;
+	var previousY:Float;
+	var wasColliding:Bool = false;
+
 	override function update(dt:Float)
 	{
-		super.update(dt);
+    	previousX = x;
+    	previousY = y;
 
-		if (FlxCollision.pixelPerfectCheck(this, Map.instance.mapSpr))
-		{
+    	super.update(dt);
+
+    	var collision = FlxCollision.pixelPerfectCheck(this, Map.instance.mapSpr);
+
+    	if (collision && !wasColliding)
+    	{
+        	x = previousX;
+        	y = previousY;
+
 			FlxG.sound.play(Paths.sound('hit'));
 
-			velocity.x *= -1;
-			velocity.y *= -1;
-		}
+        	randomDirection();
+    	}
+
+    	wasColliding = collision;
+	}
+
+	function randomDirection()
+	{
+    	if (Math.abs(velocity.x) > Math.abs(velocity.y))
+    	{
+        	velocity.x *= -1;
+        	velocity.y = FlxG.random.float(-speed, speed);
+    	}
+    	else
+    	{
+        	velocity.y *= -1;
+        	velocity.x = FlxG.random.float(-speed, speed);
+    	}
+
+    	var length = Math.sqrt(velocity.x * velocity.x + velocity.y * velocity.y);
+
+    	velocity.x = velocity.x / length * speed;
+    	velocity.y = velocity.y / length * speed;
 	}
 }
