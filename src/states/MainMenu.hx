aa9c444654810dcef455fb4e28966d853b76340f
sammieGHA <oo650869@gmail.com>
2026-08-18 15:55:28 -0500

    make gtrid better later

--- src/states/MainMenu.hx
+++ src/states/MainMenu.hx
@@ -3,6 +3,8 @@ package states;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.addons.display.FlxBackdrop;
+import flixel.addons.display.FlxGridOverlay;
 import flixel.tweens.FlxEase;
 import flixel.util.FlxColor;
 import flixel.util.FlxGradient;
@@ -22,6 +24,10 @@ class MainMenu extends FlxState
 		// make it better
 		var bg = FlxGradient.createGradientFlxSprite(FlxG.width, FlxG.height, [FlxColor.BLACK, FlxColor.GRAY], 1, 45, true);
 		add(bg);
+
+		var grid = new FlxBackdrop(FlxGridOverlay.createGrid(125, 88, 250, 175, true, 0x55FFFFFF, 0x3FFFFFFF));
+		grid.velocity.set(-40, -40);
+		add(grid);
 	}
 
 	override function update(dt:Float)
